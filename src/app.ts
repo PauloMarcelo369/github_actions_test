c78b5f0badbe4fb27271738c9c5b7d223ec88b91
Paulo Marcelo <marcelomp97447@gmail.com>
2026-01-30 10:36:04 -0300

    feat: deploy success!! :)

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.json("aplicativo funcionando de boa!");
+  res.json("aplicativo funcionando de boa! :) :) :)");
 });
 
 export default app;
